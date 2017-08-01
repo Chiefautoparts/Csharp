1de75717d3bb059551a7bf15733c00e5f7d3b660
chiefautoparts <chiefautoparts@outllook.com>
2017-08-01 13:20:31 -0700

    group project animal kingdom

---
+++ animalKingdom/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace animalKingdom
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            animal Dog = new animal(4);
+            Console.WriteLine("This Animal has " + Dog.numlegs + " legs");
+            // Dog.Dog(4, "steve", 20);
+            // Console.WriteLine("the number of legs is " + Dog.Numlegs);
+
+            // Console.WriteLine(Dog.Numlegs);
+            // Dog bob = new Dog();
+            // Console.WriteLine();
+            // bob.speak("woof");
+            // Console.WriteLine();
+        }
+    }
+}
