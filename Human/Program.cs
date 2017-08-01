3e991e7fb9860a2cb957b3700b70bc5df81c9d7a
chiefautoparts <chiefautoparts@outllook.com>
2017-08-01 15:00:44 -0700

    human assign

---
+++ Human/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Human person1 = new Human("tom", 9, 2, 4, 250);
+         System.Console.WriteLine(person1.name);
+         person1.attack("bill");
+        }
+    }
+    public class Human
+    {
+        public Human(string name, int strength, int intelligence, int dexterity, int health) 
+        {
+            this.name = name;
+            this.strength = strength;
+            this.intelligence = intelligence;
+            this.dexterity = dexterity;
+            this.health = health;
+               
+        }
+        public string name { get; set; }
+        public int strength { get; set; }
+        public int intelligence { get; set; }
+        public int dexterity { get; set; }
+        public int health { get; set; } 
+        // public People(string newName)
+        // {
+        //     name = newName;
+        // }
+        public int damage()
+        {
+            return strength * 5;
+        }
+        // public void attack(object target)
+        // {
+        //     enemy = Human;
+        //     enemy = target;
+        //     if(enemy != null); 
+        //     {
+        //        enemy.hp -= damage;
+        //     }
+        // }
+    }
+
+}
---
+++ classesPreactice/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace classesPreactice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Vehicle myVehicle = new Vehicle(7);
+            Console.WriteLine("My vehicle can hold" + myVehicle.numPassengers + " people");
+            Vehicle car = new Vehicle(5);
+            Vehicle bike = new Vehicle(1);
+            Console.WriteLine(car.distance);
+            Console.WriteLine(bike.distance);
+            car.Move(70.8);
+            Console.WriteLine(car.distance);
+            Console.WriteLine(bike.distance);
+        }
+    }
+    public class Vehicle
+    {
+        public int numPassengers;  
+        public double distance = 0.0;
+        public Vehicle(int val)
+        {
+            numPassengers = val;
+        }
+        public Vehicle(int val, double odo)
+        {
+            numPassengers = val;
+            distance = odo;
+        }
+        public int Move(double miles)
+        {
+            distance += miles;
+            return (int)distance;
+        }
+        public int Move(double miles, bool km)
+        {
+            if (km == true)
+            {
+                miles = miles * 0.62;
+            }
+            return Move(miles);
+        }
+    }
+}
