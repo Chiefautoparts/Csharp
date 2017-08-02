e6da6d3b7aac267becc127f46f236b0384be1b14
chiefautoparts <chiefautoparts@outllook.com>
2017-08-02 11:42:59 -0700

    added Cards for stuff

--- DeckofCards/Deck.cs
+++ DeckofCards/Deck.cs
@@ -5,19 +5,32 @@ namespace DeckofCards
     public class Deck
     {
         List<Card> cards;
-    }
-
-    public Deck reset()
-    {
-        string[] suites = {"hearts", "diamonds", "spades", "clubs"};
-        string[] strVals = {"Ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"};
-        foreach(string Suit in suits)
+        public Deck();
         {
-            for (int i = 0; i<strVals.Length; i++)
-            {
-                Card noob = new Card(strVals[i], Suit, i+1);
-                
-            }
+            reset();
+            shuffle();
         }
+
+        public Deck reset()
+        {
+            cards = new List<Card>();
+            string[] suits = {"hearts", "diamonds", "spades", "clubs"};
+            string[] strVals = {"Ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"};
+            foreach(string Suit in suits)
+            {
+                for (int i = 0; i<strVals.Length; i++)
+                {
+                    Card Foo = new Card(strVals[i], Suit, i+1);
+                    cards.Add(Foo);
+                
+                }
+            }
+            return this;
+        }
+    }
+    public Deck shuffle()
+    {
+        cards = new List<Card>();
+
     }
 }
