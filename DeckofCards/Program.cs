ffbb9b019e7a2b7d7eef8c003c20fce46874b513
chiefautoparts <chiefautoparts@outllook.com>
2017-08-01 18:56:36 -0700

    wizard, ninja work

---
+++ DeckofCards/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DeckofCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card jon = new Card("jon", "black", 1);
+            System.Console.WriteLine(jon.val);
+        }
+    }
+}
--- Human/Program.cs
+++ Human/Program.cs
@@ -6,44 +6,9 @@ namespace Human
     {
         static void Main(string[] args)
         {
-         Human person1 = new Human("tom", 9, 2, 4, 250);
-         System.Console.WriteLine(person1.name);
-         person1.attack("bill");
+         Human person1 = new Human("Tim", 200, 8, 2, 4);
+         Console.WriteLine(person1.name + " has " + person1.health + " Health points");
         }
-    }
-    public class Human
-    {
-        public Human(string name, int strength, int intelligence, int dexterity, int health) 
-        {
-            this.name = name;
-            this.strength = strength;
-            this.intelligence = intelligence;
-            this.dexterity = dexterity;
-            this.health = health;
-               
-        }
-        public string name { get; set; }
-        public int strength { get; set; }
-        public int intelligence { get; set; }
-        public int dexterity { get; set; }
-        public int health { get; set; } 
-        // public People(string newName)
-        // {
-        //     name = newName;
-        // }
-        public int damage()
-        {
-            return strength * 5;
-        }
-        // public void attack(object target)
-        // {
-        //     enemy = Human;
-        //     enemy = target;
-        //     if(enemy != null); 
-        //     {
-        //        enemy.hp -= damage;
-        //     }
-        // }
     }
 
 }
